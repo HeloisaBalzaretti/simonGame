a45d581793ae5c7826364b558b36620f1e63e367
Heloisa Balzaretti <heloisa.cardosods@gmail.com>
2021-01-15 18:11:13 +1300

    CatSimon

--- game.js
+++ game.js
@@ -1,8 +1,10 @@
 var gameSequencePlayed = [];
 var userSequencePlayed = [];
-var buttonColors = ["red", "blue", "green", "yellow"]; 
+var buttonColors = ["red", "blue", "green", "yellow"];
+var catSongs = ["cat1", "cat2", "cat3", "cat4"];
 var isGameOn = false;
 var isSimonTurn = true;
+const MOVES_TIMEOUT = 1000;
 //general functions 
 
 function getRandomNumber() {
@@ -17,13 +19,16 @@ function getNextColor() {
 
 function playSound(buttonColor){
   //receives a color and plays the sound. eg playSound(getNextColor())
+  if (buttonColor != 'wrongCat'){
+    buttonColor = catSongs[buttonColors.indexOf(buttonColor)];
+  }
   var colorSound = new Audio(`sounds/${buttonColor}.mp3`);
   colorSound.play();
 }
 
 function playWrongSound(){
   //plays the error sound.
-  playSound('wrong');
+  playSound('wrongCat');
 }
 function blinkButton(btnColor){
   $(`#${btnColor}`).fadeOut(100).fadeIn(100);
@@ -40,7 +45,7 @@ function showCurrentMoves(index){
       blinkButton(color);
       playSound(color);
       showCurrentMoves(index + 1);
-    }, 500);
+    }, MOVES_TIMEOUT);
   } else {
     simonPlays();
   }
@@ -60,7 +65,7 @@ function simonPlays(){
     blinkButton(color);
     playSound(color);
     isSimonTurn = false;
-  }, 500);
+  }, MOVES_TIMEOUT);
 }
 
 function userPlays(colorClicked){ 
@@ -74,7 +79,8 @@ function userPlays(colorClicked){
       simonTurn();
     }    
   } else{
-    $("#level-title").text("You lost! Press any key to play again!");
+    $("#level-title").html('You lost! click <a class="underline">here</a> to play again!');
+    $(".underline").click(startGame);
     blinkButton(colorClicked);
     playWrongSound();
     isGameOn = false;
